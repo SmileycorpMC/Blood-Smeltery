929125cfd6e9c6e6dc7245a4f9fd669551fdb6ef
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-07-31 16:21:43 +0100

    fix divination model rendering

--- src/main/java/net/smileycorp/bloodsmeltery/mixin/MixinItemRenderer.java
+++ src/main/java/net/smileycorp/bloodsmeltery/mixin/MixinItemRenderer.java
@@ -27,15 +27,18 @@ public abstract class MixinItemRenderer {
 	@Shadow public abstract void render(ItemStack p_115144_, ItemDisplayContext p_270188_, boolean p_115146_, PoseStack p_115147_, MultiBufferSource p_115148_, int p_115149_, int p_115150_, BakedModel p_115151_);
 
 	@Inject(at=@At("HEAD"), method = "render")
-	public void bloodsmeltery$render(ItemStack stack, ItemDisplayContext ctx, boolean p_115146_, PoseStack p_115147_, MultiBufferSource p_115148_, int p_115149_, int p_115150_, BakedModel p_115151_, CallbackInfo callback) {
+	public void bloodsmeltery$render(ItemStack stack, ItemDisplayContext ctx, boolean p_115146_, PoseStack poseStack, MultiBufferSource p_115148_, int p_115149_, int p_115150_, BakedModel p_115151_, CallbackInfo callback) {
 		if (ctx != ItemDisplayContext.GUI || stack == null) return;
 		if (!(stack.getItem() instanceof IModifiable)) return;
 		ToolStack tool = ToolStack.from(stack);
 		if (tool == null) return;
 		int divinationLevel = tool.getModifierLevel(ModContent.DIVINATION.get());
-		if (divinationLevel < 0) return;
+		if (divinationLevel < 1) return;
 		ItemStack sigil = new ItemStack(divinationLevel > 1 ? BloodMagicItems.SEER_SIGIL.get() : BloodMagicItems.DIVINATION_SIGIL.get());
-		render(sigil, ctx, p_115146_, p_115147_, p_115148_, p_115149_, p_115150_, getModel(sigil, null, null, 0));;
+		poseStack.pushPose();
+		poseStack.translate(0, 0, -1);
+		render(sigil, ctx, p_115146_, poseStack, p_115148_, p_115149_, p_115150_, getModel(sigil, null, null, 0));
+		poseStack.popPose();
 	}
 
 }
