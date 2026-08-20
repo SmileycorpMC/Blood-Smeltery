46ef6cd19d4730a463a1c87671056ab312135dfb
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-08-20 17:39:46 +0100

    finish hellfire forge fluid capability

--- src/main/java/net/smileycorp/bloodsmeltery/common/util/HellfireForgeFluidCapability.java
+++ src/main/java/net/smileycorp/bloodsmeltery/common/util/HellfireForgeFluidCapability.java
@@ -1,15 +1,21 @@
 package net.smileycorp.bloodsmeltery.common.util;
 
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.smileycorp.bloodsmeltery.common.BloodSmeltery;
 import wayoftime.bloodmagic.api.compat.EnumDemonWillType;
+import wayoftime.bloodmagic.api.compat.IDemonWillGem;
 import wayoftime.bloodmagic.common.tile.TileSoulForge;
+
+import javax.annotation.Nullable;
+import java.util.Optional;
 
 public class HellfireForgeFluidCapability implements IFluidHandler, ICapabilityProvider {
 
@@ -19,59 +25,56 @@ public class HellfireForgeFluidCapability implements IFluidHandler, ICapabilityP
 		this.tile = tile;
 	}
 
-	protected FluidStack getFluid() {
-		for (int i = 0; i < 5; i++) {
-			BloodSmeltery.logInfo(tile.getItem(i));
-		}
-		return FluidStack.EMPTY;
-	}
-
-	private void setType(EnumDemonWillType type) {
-
-	}
-
 	@Override
 	public int getTanks() {
-		return 1;
+		IFluidHandlerItem gem = getTartaricGem();
+		return gem == null ? 1 : gem.getTanks();
 	}
 
 	@Override
 	public FluidStack getFluidInTank(int tank) {
-		return getFluid();
+		IFluidHandlerItem gem = getTartaricGem();
+		return gem == null ? FluidStack.EMPTY : gem.getFluidInTank(tank);
 	}
 
 	@Override
 	public int getTankCapacity(int tank) {
-		return 1000;
+		IFluidHandlerItem gem = getTartaricGem();
+		return gem == null ? 0 : gem.getTankCapacity(tank);
 	}
 
 	@Override
 	public boolean isFluidValid(int tank, FluidStack stack) {
-		for (int i = 0; i < 5; i++) {
-			BloodSmeltery.logInfo(tile.getItem(i));
-		}
-		return false;
+		IFluidHandlerItem gem = getTartaricGem();
+		return gem != null && gem.isFluidValid(tank, stack);
 	}
 
 	@Override
 	public int fill(FluidStack resource, FluidAction action) {
-		return 0;
+		IFluidHandlerItem gem = getTartaricGem();
+		return gem == null ? 0 : gem.fill(resource, action);
 	}
 
 	@Override
 	public FluidStack drain(FluidStack resource, FluidAction action) {
-		return FluidStack.EMPTY;
+		IFluidHandlerItem gem = getTartaricGem();
+		return gem == null ? FluidStack.EMPTY : gem.drain(resource, action);
 	}
 
 	@Override
 	public FluidStack drain(int maxDrain, FluidAction action) {
-		return FluidStack.EMPTY;
+		IFluidHandlerItem gem = getTartaricGem();
+		return gem == null ? FluidStack.EMPTY : gem.drain(maxDrain, action);
 	}
-
 
 	@Override
 	public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
 		return cap == ForgeCapabilities.FLUID_HANDLER ? LazyOptional.of(() -> this).cast() : LazyOptional.empty();
+	}
+
+	@Nullable
+	public IFluidHandlerItem getTartaricGem() {
+		return tile.getItem(TileSoulForge.soulSlot).getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM).orElse(null);
 	}
 
 }
