83006cb990e399f4d8aa566abf25c862f975740e
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-08-20 23:18:52 +0100

    divination armour keybind support

--- src/main/java/net/smileycorp/bloodsmeltery/common/modifiers/DivinationModifier.java
+++ src/main/java/net/smileycorp/bloodsmeltery/common/modifiers/DivinationModifier.java
@@ -1,32 +1,30 @@
 package net.smileycorp.bloodsmeltery.common.modifiers;
 
-import com.google.common.collect.Lists;
-import com.mojang.authlib.GameProfile;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextColor;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.smileycorp.bloodsmeltery.common.ModContent;
+import slimeknights.mantle.client.TooltipKey;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.library.modifiers.ModifierHooks;
 import slimeknights.tconstruct.library.modifiers.hook.interaction.GeneralInteractionModifierHook;
 import slimeknights.tconstruct.library.modifiers.hook.interaction.InteractionSource;
+import slimeknights.tconstruct.library.modifiers.hook.interaction.KeybindInteractModifierHook;
 import slimeknights.tconstruct.library.module.ModuleHookMap;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
-import wayoftime.bloodmagic.core.data.Binding;
 import wayoftime.bloodmagic.util.ChatUtil;
 import wayoftime.bloodmagic.util.helper.NetworkHelper;
 
-import java.util.List;
-
-public class DivinationModifier extends Modifier implements GeneralInteractionModifierHook {
+public class DivinationModifier extends Modifier implements GeneralInteractionModifierHook, KeybindInteractModifierHook {
 
 	@Override
 	protected void registerHooks(ModuleHookMap.Builder builder) {
 		super.registerHooks(builder);
-		builder.addHook(this, ModifierHooks.GENERAL_INTERACT);
+		builder.addHook(this, ModifierHooks.GENERAL_INTERACT, ModifierHooks.ARMOR_INTERACT);
 	}
 
 	@Override
@@ -46,16 +44,15 @@ public class DivinationModifier extends Modifier implements GeneralInteractionMo
 		return InteractionResult.PASS;
 	}
 
+	@Override
+	public boolean startInteract(IToolStackView tool, ModifierEntry modifier, Player player, EquipmentSlot slot, TooltipKey keyModifier) {
+		sendMessage(modifier, player);
+		return false;
+	}
+
 	public static void sendMessage(ModifierEntry modifier, Player player) {
-		int level = modifier.getLevel();
-		String key = "tooltip.bloodmagic.sigil." + (level > 1 ? "divination" : "seer") + ".";
-		GameProfile profile = player.getGameProfile();
-		Binding binding = new Binding(profile.getId(), profile.getName());
-		int currentEssence = NetworkHelper.getSoulNetwork(binding).getCurrentEssence();
-		List<Component> message = Lists.newArrayList();
-		if (!binding.getOwnerId().equals(player.getGameProfile().getId())) message.add(Component.translatable(key + "otherNetwork", binding.getOwnerName()));
-		message.add(Component.translatable(key + "currentEssence", currentEssence));
-		ChatUtil.sendNoSpam(player, message.toArray(new Component[message.size()]));
+		ChatUtil.sendNoSpam(player, Component.translatable("tooltip.bloodmagic.sigil." + (modifier.getLevel() > 1 ? "divination" : "seer")
+						+ ".currentEssence", NetworkHelper.getSoulNetwork(player).getCurrentEssence()));
 	}
 
 }
